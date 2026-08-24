be25b7a41d884d6bf3caaa66d1b4c0b4ce0927bc
herrdelta83 <bailondelta@gmail.com>
2026-08-24 12:27:36 -0600

    Updated solution

--- 217/main.cpp
+++ 217/main.cpp
@@ -1,17 +1,30 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-const int MOD = 1000000007;
-
 class Solution {
 public:
-     
-}
+    void containsDuplicate(vector<int>& nums) {
+        //initialize empty hash set
+        unordered_set<int> us;
+        //search through nums vector
+        for(auto x : nums){
+            auto it = us.find(x); // O(1)
+            if (it != us.end())
+                cout << "true" << endl;
+            else 
+                us.insert(x);          
+        }
+    }
+};
 
-signed main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
-
+int main() {
+    Solution sol;
+    int n;
+    cin>>n;
+    vector<int> nums (n);
+    for (int i = 0; i < n; i++){
+        cin >> nums[i];
+    }
+    sol.containsDuplicate(nums);
     return 0;
 }
