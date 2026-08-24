46a79ddc8ed1cb24ff148abea43e690a4df7ce3c
herrdelta83 <bailondelta@gmail.com>
2026-08-24 10:13:59 -0600

    Add templates for C++ & Python

---
+++ template/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    void methodName() {
+
+    }
+};
+
+int main() {
+    Solution sol;
+    sol.methodName();
+    return 0;
+}
---
+++ template/main.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def methodName(self, ):
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.methodName())  # Expected:
